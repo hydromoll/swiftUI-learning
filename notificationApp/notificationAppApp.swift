961533949062df4a2b6609d32b5e51fc233ffb28
hydromoll <hydromoll@mail.ru>
2022-10-02 18:26:02 +0300

    Initial Commit

---
+++ notificationApp/notificationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  notificationAppApp.swift
+//  notificationApp
+//
+//  Created by hydromoll on 19.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct notificationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
